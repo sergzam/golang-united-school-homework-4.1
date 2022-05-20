46adaa274ad50619fed375fa8fc424efac25c2ac
Sergii Zamashnykh <Sergii.Zamashnykh@refinitiv.com>
2022-05-20 10:40:06 +0300

    finished

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,10 @@
 package reverse_string
 
-func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+func ReverseString(input string) string {
+	r := []rune(input)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+
+	return string(r)
 }
